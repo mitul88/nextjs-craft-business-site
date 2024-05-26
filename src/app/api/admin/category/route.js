0112d7d72948c533f9b0e3629a4c800a96901cf7
mitul88 <shahed.rahman88@gmail.com>
2024-05-27 00:29:15 +0600

    category img upload done

--- src/app/api/admin/category/route.js
+++ src/app/api/admin/category/route.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+import { writeFile } from "fs/promises";
+import path from "path";
 
 const prisma = new PrismaClient();
 
@@ -22,9 +24,27 @@ export async function GET(req) {
 }
 export async function POST(req) {
   try {
-    const categoryData = await req.json();
+    const formData = await req.formData();
+    const categoryName = formData.get("name");
+    const file = formData.get("img");
+
+    if (!file) {
+      return NextResponse.json(
+        { message: "image is not provided" },
+        { status: 400 }
+      );
+    }
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const filename = Date.now() + file.name.replaceAll(" ", "_");
+
+    const uploadDir = path.join(process.cwd(), "public/uploads/" + filename);
+
+    await writeFile(uploadDir, buffer);
     await prisma.category.create({
-      data: categoryData,
+      data: {
+        name: categoryName,
+        img: uploadDir,
+      },
     });
     return NextResponse.json({ message: "category created" }, { status: 201 });
   } catch (error) {
