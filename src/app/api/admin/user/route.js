473dd90d3801732480d2a9bf5755109cefa4f8bc
mitul88 <shahed.rahman88@gmail.com>
2024-05-27 19:38:35 +0600

    user created api done

--- src/app/api/admin/user/route.js
+++ src/app/api/admin/user/route.js
@@ -1,21 +1,31 @@
 import { JWThelper } from "@/utils/JWTherlper";
+import { passwordHelper } from "@/utils/passwordHelper";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 export async function POST(req, res) {
   try {
-    const userData = req.json();
+    const userData = await req.json();
+    const userPassword = userData["password"];
+    const userRole = userData["role"];
+    if (!userData["email"] || !userData["password"]) {
+      return NextResponse.json(
+        { message: "please provide all required information" },
+        { status: 400 }
+      );
+    }
+    const hashedPassword = await passwordHelper.hashPassword(userPassword);
     await prisma.user.create({
       data: {
         email: userData["email"],
-        password: userData["password"],
-        role: userData["role"],
+        password: hashedPassword,
+        role: userRole.toUpperCase(),
       },
     });
     return NextResponse.json({ message: "user created" }, { status: 201 });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return NextResponse.json(
       { message: "an internal error occured" },
       { status: 500 }
--- src/utils/passwordHelper.js
+++ src/utils/passwordHelper.js
@@ -1,8 +1,9 @@
 import bcrypt from "bcrypt";
 export const passwordHelper = {
   hashPassword: async (userPass) => {
+    const userPassword = userPass.toString();
     const salt = await bcrypt.genSalt(10);
-    const password = await bcrypt.hash(userPass, salt);
+    const password = await bcrypt.hash(userPassword, salt);
     return password;
   },
   verifyPassword: async (passwordFromClient, verifyWith) => {
