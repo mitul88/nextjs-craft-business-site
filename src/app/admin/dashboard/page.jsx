1369e7b9630ea2867fb6ba70989c8f75af2325a3
mitul88 <shahed.rahman88@gmail.com>
2024-07-02 19:29:35 +0600

    dashboard layout added

---
+++ src/app/admin/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import DashboardMasterLayout from "@/layout/DashboardMasterLayout";
+import React from "react";
+
+const page = () => {
+  return (
+    <main>
+      <DashboardMasterLayout>
+        Hello from dashboard route using dashboard master layout
+      </DashboardMasterLayout>
+    </main>
+  );
+};
+
+export default page;
---
+++ src/layout/DashboardMasterLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DashboardMasterLayout = ({ children }) => {
+  return (
+    <section>
+      <div></div>
+      <div>{children}</div>
+    </section>
+  );
+};
+
+export default DashboardMasterLayout;
